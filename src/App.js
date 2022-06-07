7cef5bf1199c51faa8df6f1444be9e5748f92602
erfanmoulaee <erfanmoulaee1375@gmail.com>
2022-06-07 23:56:11 +0430

    local storage added and todolist is finished

--- src/App.js
+++ src/App.js
@@ -6,12 +6,26 @@ import TodoList from "./components/TodoList";
 
 function App() {
   const [inputText, setInputText] = useState("");
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(() => {
+    // get the todos from localstorage
+    const savedTodos = localStorage.getItem("todos");
+    // if there are todos stored
+    if (savedTodos) {
+      // return the parsed JSON object back to a javascript object
+      return JSON.parse(savedTodos);
+      // otherwise
+    } else {
+      // return an empty array
+      return [];
+    }
+  });
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
+  // RUN ONCE WHEN THE APP START
 
   useEffect(() => {
     filterHandler();
+    saveLocalTodos();
   }, [todos, status]);
 
   const filterHandler = () => {
@@ -27,6 +41,11 @@ function App() {
         break;
     }
   };
+  //save to local
+  const saveLocalTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
   return (
     <div className="App">
       <header>
